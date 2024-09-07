20ba0cde8296542bae55ab51be02064f681315ce
Olga Khamtsova <khamtsovaolga@gmail.com>
2024-09-07 13:42:56 +0300

    Initial commit

---
+++ src/main/java/org/example/PhoneBook.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Map;
+
+public class PhoneBook {
+    public Map mapPhoneBook;
+
+    public PhoneBook(Map mapPhoneBook) {
+        this.mapPhoneBook = mapPhoneBook;
+    }
+}
---
+++ src/test/java/PhoneBookTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.example.PhoneBook;
+
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PhoneBookTest {
+
+}
